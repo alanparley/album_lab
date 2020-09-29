f275f1facdba656fe8c4ac1270b49e8eb682102e
Alan Parley <alanparley@hotmail.com>
2020-09-29 16:36:18 +0100

    initial commit

---
+++ db/our_price.sql
@@ -0,0 +1,13 @@
+DROP TABLE IF EXISTS albums;
+DROP TABLE IF EXISTS artists;
+
+CREATE TABLE artists(
+    id SERIAL PRIMARY KEY,
+    artist_name VARCHAR(255)
+);
+
+CREATE TABLE albums (
+    id SERIAL PRIMARY KEY,
+    album_title VARCHAR(255),
+    album_genre VARCHAR(255)
+);
---
+++ models/album.py
@@ -0,0 +1,8 @@
+class Album:
+
+    def __init__ (self, album_title, album_genre, id = None):
+        self.album_title = album_title
+        self.album_genre = album_genre
+        self.id = id
+
+        
